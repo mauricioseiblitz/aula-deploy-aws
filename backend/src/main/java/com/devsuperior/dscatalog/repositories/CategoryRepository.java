b9f9c58996d971c87e226ea93165d43f83992a53
Mauricio3 <mauricio-monteiro@hotmail.com.br>
2021-11-27 21:00:54 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+//Camada de Acesso a dados
+
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
