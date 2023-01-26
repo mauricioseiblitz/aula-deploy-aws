89a269750399f6748958528a578ef37630aa913d
Mauricio3 <mauricio-monteiro@hotmail.com.br>
2023-01-26 20:09:08 -0300

    Fix navbar style

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                 <a href="link" className="nav-logo-text">
                     <h4>DS Catalog</h4>
                 </a>
-                <div className="collpase navbar-collapse">
+                <div className="collapse navbar-collapse">
                     <ul className="navbar-nav offset-md-2 main-menu">
                         <li>
                             <a href="link" className="active">HOME</a>
