b04c50a362e56f0d69fef9ba0074ad706f34e63d
Mauricio3 <mauricio-monteiro@hotmail.com.br>
2023-02-13 14:17:24 -0300

    Home styling

---
+++ frontweb/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const ButtonIcon = () => {
+    return (
+        <button className="btn btn-primary btn-icon">
+            <h5>INICIE AGORA A SUA BUSCA</h5>
+        </button>
+    );
+}
+
+export default ButtonIcon;
