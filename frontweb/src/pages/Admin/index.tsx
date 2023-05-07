e34d5fd58002ae69fc11ef849c4d03bd415c2bab
Mauricio3 <mauricio-monteiro@hotmail.com.br>
2023-05-06 22:46:46 -0300

    Admin Navbar layout mobile

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,6 +1,15 @@
+import Navbar from './Navbar';
+
+import './styles.css';
+
 const Admin = () => {
     return (
-        <h1>Página de Admin</h1>
+        <div className="admin-container">
+            <Navbar />
+            <div className="admin-content">
+                <h1>Conteúdo</h1>
+            </div>
+        </div>
     );
 }
 
